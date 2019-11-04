fab3c04199d778ac230557cabe7feb8fed5f4624
leonardomoreno1694 <48774568+leonardomoreno1694@users.noreply.github.com>
2019-11-04 20:03:59 -0300

    Create tododb_dml.sql

---
+++ tododb_dml.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `persona` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `alias` varchar(25) NOT NULL,
+ `nombre` varchar(25) NOT NULL,
+ `apellido` varchar(25) DEFAULT NULL,
+ `email` varchar(90) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1
+
+
+
+CREATE TABLE `tarea` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `descripcion` varchar(200) NOT NULL,
+ `fecha_limite` date DEFAULT NULL,
+ `persona_id` int(11) NOT NULL,
+ `realizada` tinyint(1) NOT NULL,
+ `fecha_realizada` date NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `persona_id` (`persona_id`),
+ CONSTRAINT `tarea_ibfk_1` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
